c9910e9588362c26cbd6af5811207be0f7bd335d
mostafa <mt.khoshnood@gmail.com>
2023-06-07 05:14:45 +0330

    main file changed!

--- application/crud/courses.py
+++ application/crud/courses.py
@@ -2,7 +2,7 @@ from application.core.db import database
 from application.schemas import Course
 
 async def get_all_courses():
-    query='SELECT * FROM Course'
+    query='SELECT Course.crs_id,Course.crs_title,Course.dep_id,Instructor.ins_fname,Instructor.ins_lname FROM Course INNER JOIN Instructor ON Course.ins_id=Instructor.ins_id'
     data = await database.fetch_all(query)
 
     return data
--- application/main.py
+++ application/main.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
 from application.core.db import database
 from application.router import router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.include_router(router)
 sleep_time = 10
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.on_event("startup")
 async def startup():
     await database.connect()
